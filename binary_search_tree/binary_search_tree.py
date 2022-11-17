8a52a4ca98b0509c1932c3e27cb089c89fdadd8b
Ozlem E <ozlemekici9774@gmail.com>
2022-11-17 13:18:54 +0300

    create

---
+++ binary_search_tree/binary_search_tree.py
@@ -0,0 +1,23 @@
+# [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+# Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+
+# İlk olarak root'u yerleştiririz. Ardından dizinin 2. elemanının root'dan büyük mü yoksa küçük mü olduğunu kontrol
+# ederiz. Büyükse ağacın root'umuzun altındaki sağ dalına, küçükse sol dalına yerleştiririz. Bu şekilde diğerlerini de
+# sırasıyla yerleştirip ağacımızı oluştururuz.
+
+# Öncelikle dengeli bir ağacımız olması yani average case durumumuza bakalım
+# Root = 7
+
+#          7
+#        /   \
+#       5     8
+#     /  \     \
+#    1    6     9
+#   / \
+#  0   3
+#      /\
+#     2  4
+
+# Bu durumda Big-O gösterimimiz: O(logn) şeklindedir
