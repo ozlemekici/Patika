ad650e175b0a080ae4a2663c2f5d7f9923d90b31
Ozlem E <ozlemekici9774@gmail.com>
2022-11-17 13:24:31 +0300

    fixed

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,5 +1,4 @@
 # [16,21,11,8,12,22] -> Merge Sort
-
 # Yukarıdaki dizinin sort türüne göre aşamalarını yazınız.
 # Big-O gösterimini yazınız.
 
@@ -12,6 +11,9 @@
 #     [11,16,21]      [8,12,22]
 #        [8,11,12,16,21,22]
 
-# Sayı dizimiz değişmediğinden, sadece bölmelere ayırdığımızdan ve her kademede taradığımız eleman sayısı aynı olduğundan
-# O(n) olur. Bu O(n) işlemini de 2^x = n'den logn = x yani logn kere yaparız. Bu nedenle Big-O gösterimi: O(nlogn)
-# şeklindedir
+
+
+
+# Sayı dizimiz değişmediğinden, sadece bölmelere ayırdığımızdan ve her kademede taradığımız eleman sayısı 
+# aynı olduğundan O(n) olur. Bu O(n) işlemini de 2^x = n'den logn = x yani logn kere yaparız. 
+# Bu nedenle Big-O gösterimi: O(nlogn) şeklindedir
